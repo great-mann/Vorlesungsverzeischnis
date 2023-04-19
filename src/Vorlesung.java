6df2d8733b69078d504bb88ef8d4029ba96d0020
andy <an6839@stud.hs-emden-leer.de>
2023-04-19 12:56:47 +0200

    Vorlesung

--- src/Vorlesung.java
+++ src/Vorlesung.java
@@ -1,4 +1,16 @@
 public class Vorlesung {
+    String sg;
+    String titel;
+    String dozent;
+    int anzahl;
+    Vorlesung(String sg, String titel, String dozent, int anzahl){
+        this.sg = sg;
+        this.titel = titel;
+        this.dozent = dozent;
+        this.anzahl = anzahl;
+
+
+    }
 
 
 }
