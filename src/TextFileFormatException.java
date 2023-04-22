e40d19481b44e777eecdedd5231d9ba8d2ae7497
andy <an6839@stud.hs-emden-leer.de>
2023-04-22 18:55:42 +0200

    konstruktor + fehlermeldung

--- src/TextFileFormatException.java
+++ src/TextFileFormatException.java
@@ -1,24 +1,8 @@
-public class TextFileFormatException extends Exception {
-    //TODO , wenn mehr als 4 Spalten
-    public TextFileFormatException(int error) {
-        switch(error) {
-            case 1:
-                // Spaltenanzahl zu viel/zu wenig
-                System.out.println("Die Spaltenanzahl beträgt nicht 4");
-                break;
-            case 2:
-                // Zu wenig Informationen
-                System.out.println("Teile der Spalten haben keinen Inhalt");
-                break;
-            case 3:
-                // Formatfehler
-                System.out.println("In der Tabelle sind Formatfehler");
-                break;
-            default:
-                // Falscher Fehlercode
-                System.out.println("Falscher Fehlercode");
-        }
+public class TextFileFormatException extends IllegalArgumentException {
+    public TextFileFormatException(String error) {
 
-       // System.out.println(error);
+        super(error);
+
     }
+
 }
